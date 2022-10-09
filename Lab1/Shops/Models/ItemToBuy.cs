2cc1fb04936fad6bd123ade307ec6b3765e44bae
Michael Makarov <idigitallegacy@yandex.ru>
2022-10-09 21:37:53 +0300

    fix:Naming issue caused by Product class split for Product and ServiceProduct

--- Lab1/Shops/Models/ItemToBuy.cs
+++ Lab1/Shops/Models/ItemToBuy.cs
@@ -4,11 +4,11 @@ namespace Shops.Models;
 
 public class ItemToBuy
 {
-    public ItemToBuy(Product serviceProduct, int preferredAmount)
+    public ItemToBuy(Product product, int preferredAmount)
     {
         if (preferredAmount < 0)
             throw new ProductAmountException("Unable to generate item to buy: preferred amount must be above 0.");
-        Product = serviceProduct;
+        Product = product;
         PreferredAmount = preferredAmount;
     }
 
